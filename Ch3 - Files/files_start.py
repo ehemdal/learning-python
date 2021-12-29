377b08aecc5474cf94b399aab81612a9e120ba80
ehemdal <erik.hemdal@gmail.com>
2021-12-29 16:27:17 -0500

    Adding file and ospath exercises

--- Ch3 - Files/files_start.py
+++ Ch3 - Files/files_start.py
@@ -9,15 +9,24 @@ def main():
 
     
     # Open the file for appending text to the end
-
+    file = open("file.txt","a+")
 
     # write some lines of data to the file
-
+    for i in range(10):
+        file.write("This is a line of text\n")
     
     # close the file when done
-
+    file.close()
     
     # Open the file back up and read the contents
+    myfile = open("file.txt", "r")
+    if myfile.mode == "r":
+        contents = myfile.read()
+     #   contents = myfile.readlines()
+     #   for x in contents:
+     #       print(x)
+    
+    print(contents)
 
     
 if __name__ == "__main__":
--- Ch3 - Files/ospathutils_start.py
+++ Ch3 - Files/ospathutils_start.py
@@ -12,19 +12,28 @@ import time
 
 def main():
     # Print the name of the OS
-
+    print(os.name)
     
     # Check for item existence and type
-
-    
+    print("Item exists: ", path.exists("file.txt"))
+    print("Item is a file: ", path.isfile("file.txt"))
+    print("Item is a directory: ", path.isdir("file.txt"))
     # Work with file paths
+    print("Item's path: ", path.realpath("file.txt"))
+    print("Item's relative path: ", path.relpath("file.txt"))
+    print("File's path and name: ", path.split(path.realpath("file.txt")))
 
     
     # Get the modification time
+    # Convert (ctime) to a readable string, print it out
+    t = time.ctime(path.getmtime("file.txt"))
+    print(t)
+    print(datetime.datetime.fromtimestamp(path.getmtime("file.txt")))
 
-    
     # Calculate how long ago the item was modified
-
+    td = datetime.datetime.now() - datetime.datetime.fromtimestamp(path.getmtime("file.txt"))
+    print("It has been", td, "since the file was modified")
+    print("Or,", td.total_seconds(), "seconds")
   
 if __name__ == "__main__":
     main()
