7f0e034d3ba7834ba90644727e162ae8a8e052f7
ehemdal <erik.hemdal@gmail.com>
2022-01-02 13:12:37 -0500

    Completing ch. 4 files.

--- Ch4 - Dates and Times/challenge_start.py
+++ Ch4 - Dates and Times/challenge_start.py
@@ -3,3 +3,42 @@
 #
 
 import calendar
+
+def countdays(year, month, day):
+    daycount = 0
+    c = calendar.monthcalendar(year, month)
+    for row in c:
+        if row[day] != 0:
+            daycount += 1
+    return daycount
+
+running = True
+try: 
+    while(running == True):
+        print("Enter the day of the week:")
+        print("0 - Monday")
+        print("1 - Tuesday")
+        print("2 - Wednesday")
+        print("3 - Thursday")
+        print("4 - Friday")
+        print("5 - Saturday")
+        print("6 - Sunday")
+
+        entry = input("? ")
+
+        if entry == "exit":
+            running = False
+            break
+        day = int(entry)
+        # Get the month and year
+        month = input("Enter the month 1 - 12: ")
+        month = int(month)
+        year = input("Enter the year: ")
+        year = int(year)
+
+        result = countdays(year, month, day)
+        print("There are " + str(result) + " days in the month")
+except Exception as e:
+    print("Invalid input")
+    print(e)
+
